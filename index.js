9b573f4d86d39499ffd4d14f73477df8989712b4
Aseem Baveja <abaveja97@gmail.com>
2018-02-22 15:10:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,12 +19,38 @@ app.get('/', function (req, res) {
 })
 
 // for Facebook verification
-app.get('/webhook/', function (req, res) {
+"""app.get('/webhook/', function (req, res) {
 	if (req.query['hub.verify_token'] === 'hello') {
 		res.send(req.query['hub.challenge'])
 	}
 	res.send('Error, wrong token')
-})
+})"""
+app.get('/webhook', (req, res) => {
+
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
+    
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+    
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+  
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
+  }
+});
 
 const myID = 1960455644201170;
 
